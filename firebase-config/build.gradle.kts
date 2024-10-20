3416a6ad0f8f5aa33bb086ecd1629450f5d2e6cb
Ivan <ivancaaas@gmail.com>
2024-10-20 23:25:48 +0200

    updatedminsdk

--- firebase-config/build.gradle.kts
+++ firebase-config/build.gradle.kts
@@ -24,7 +24,7 @@ android {
     namespace = "dev.gitlive.firebase.remoteconfig"
 
     defaultConfig {
-        minSdk = minSdkVersion
+        minSdk = 26
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
